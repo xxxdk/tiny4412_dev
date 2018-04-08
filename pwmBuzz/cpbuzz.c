f2e9d4f31c5925e74e9414977fe129a864358696
xxxdk <953954661@qq.com>
2018-04-08 00:58:33 -0700

    add code and Makefile

---
+++ pwmBuzz/cpbuzz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define PWM_OPEN  1
+#define PWM_STOP  0
--- pwmBuzz/kpbuzz.c
+++ pwmBuzz/kpbuzz.c
@@ -19,42 +19,115 @@
 #define NS_IN_1HZ           (1000000000UL)
 #define BUZZER_PWM_ID       0
 #define BUZZER_PWM_GPIO     EXYNOS4_GPD(0)
-#define PWM_MAJOR           237
+// #define PWM_MAJOR           237
 
 static struct pwm_device *pwm4buzzer;
 static struct semaphore lock;
 
-struct pwm_dev{
-  struct cdev cdev;
-};
-
+// struct pwm_buzzer_dev{
+//   struct cdev cdev;
+// };
 struct pwm_dev *pwm_devp;
 
-static void pwm_set_freq(unsigned long freq)
+
+static void pwm_buzzer_set_freq(unsigned long freq)
 {
-
+  int period_ns = NS_IN_1HZ / freq;
+  pwm_config(pwm4buzzer, period_ns/2, period_ns);
+  pwm_enable(pwm4buzzer);
+  s3c_gpio_cfgpin(BUZZER_PWM_GPIO, S3C_CPIO_SFN(2));
 }
 
-static pwm_stop(void)
+static pwm_buzzer_stop(void)
 {
-
+  s3c_gpio_cfgpin(BUZZER_PWM_GPIO, S3C_GPIO_OUTPUT);
+  pwm_config(pwm4buzzer, 0, NS_IN_HZ/100);
+  pwm_disable(pwm4buzzer);
 }
 
-static int pwm_open()
+static int pwm_buzzer_open(struct inode *inode, struct file *file)
 {
-
+  if(!down_trylock(&lock))
+    return 0;
+  else
+    return -EBUSY;
 }
 
-static int pwm_close()
+static int pwm_buzzer_close()
 {
-
+  up(&lock);
+  return 0;
 }
 
-static long pwm_ioctl()
+static long pwm_buzzer_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-
+  switch(cmd){
+  case PWM_IOCTL_SET_FREQ:
+    if(arg == 0)
+      return -EINVAL;
+    pwm_buzzer_set_freq(arg);
+    break;
+  case PWM_IOCTL_STOP:
+  default:
+    pwm_buzzer_stop();
+    break;
+  }
+  return 0;
 }
 
-struct file_operations pwm_fops ={
-
+struct file_operations pwm_buzzer_fops ={
+  .owner = THIS_MODULE,
+  .open = pwm_buzzer_open,
+  .release = pwm_buzzer_close,
+  .unlocked_ioctl = pwm_buzzer_ioctl,
 }
+
+struct pwm_misc_dev{
+  .minor = MISC_DYNAMIC_MINOR,
+  .name = DEV_NAME,
+  .fops = &pwm_buzzer_fops,
+}
+
+// static dev_setup_cdev()
+// {
+//   int err, devno;
+// }
+
+static __init int pwm_buzzer_init(void)
+{
+  int ret;
+  ret = gpio_requeat(BUZZER_PWM_GPIO, DEV_NAME);
+  if(ret){
+    printk(KERN_NOTICE "REQUEST GPIO %d FOR PWM FAILED\n", BUZZER_PWM_GPIO);
+    return ret;
+  }
+
+  gpio_set_value(BUZZER_PWM_GPIO, 0);
+  s3c_gpio_cfgpin(BUZZER_PWM_GPIO, S3C_GPIO_OUTPUT);
+
+  pwm4buzzer = pwm_request(BUZZER_PWM_ID, DEV_NAME);
+  if(IS_ERR(pwm4buzzer)){
+    printk(KERN_NOTICE "REQUEST PWM %d FOR %s FAILED\n", BUZZER_PWM_ID, DEV_NAME);
+    reruen -ENODEV;
+  }
+  pwm_stop();
+
+  sema_init(&lock, 1);
+  ret = misc_register(&pwm_misc_dev);
+  printk(KERN_NOTICE DEV_NAME "\tinitialized\n");
+  return ret;
+}
+
+static __exit void pwm_buzzer_exit(void)
+{
+  pwm_buzzer_stop();
+  misc_deregister(&pwm_misc_dev);
+  gpio_free(BUZZER_PWM_GPIO);
+}
+
+module_init(pwm_buzzer_init);
+module_exit(pwm_buzzer_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("X.D.KAI");
+MODULE_VERSION("20118.04.08");
