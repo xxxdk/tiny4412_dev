727d11186e2154aa1622c1dc8a932c63f256b3e0
xxxdk <953954661@qq.com>
2018-04-13 05:10:14 -0700

    rm *.o etc

--- tasklet/.ktask.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/project/workSpace/tiny4412_dev/tasklet/ktask.ko := arm-linux-ld -EL -r  -T /home/project/Downloads/linux-3.5/scripts/module-common.lds --build-id  -o /home/project/workSpace/tiny4412_dev/tasklet/ktask.ko /home/project/workSpace/tiny4412_dev/tasklet/ktask.o /home/project/workSpace/tiny4412_dev/tasklet/ktask.mod.o
--- tasklet/.tmp_versions/ktask.mod
+++
@@ -1,2 +0,0 @@
-/home/project/workSpace/tiny4412_dev/tasklet/ktask.ko
-/home/project/workSpace/tiny4412_dev/tasklet/ktask.o
